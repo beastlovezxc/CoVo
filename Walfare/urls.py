ae69d09f1e7f2390a4c4041a69615e9854caccf3
Bean <beastkillerbin@gmail.com>
2020-05-01 00:51:33 +0800

    添加福利管理相关操作

--- Walfare/urls.py
+++ Walfare/urls.py
@@ -1,7 +1,7 @@
 '''
 @Author: your name
 @Date: 2020-04-25 16:53:37
-@LastEditTime: 2020-04-30 23:43:25
+@LastEditTime: 2020-05-01 00:08:49
 @LastEditors: BeanCB
 @Description: In User Settings Edit
 @FilePath: /Covo/Walfare/urls.py
@@ -14,6 +14,9 @@ urlpatterns = [
     path('walfarelist/', views.get_walfarelist, name='walfarelist'),
     path('to_walfaremanager/', views.to_walfaremanager, name='to_walfaremanager'),
     path('add_walfare/', views.add_walfare, name='add_walfare'),
+    path('save_walfare/', views.save_walfare, name='save_walfare'),
+    path('edit_walfare/<int:prize_number>', views.edit_walfare, name='edit_walfare'),
+    path('delete_walfare/<int:prize_number>', views.delete_walfare, name='delete_walfare'),
     # path('walfaremanager/', views.walfaremanager)
     path('walfareinfo/<int:prize_number>', views.get_walfareinfo, name='walfareinfo')
 ]
--- Walfare/views.py
+++ Walfare/views.py
@@ -2,11 +2,12 @@
 @Author: BeanCB
 @Date: 2020-04-25 16:42:26
 @LastEditors: BeanCB
-@LastEditTime: 2020-04-30 23:43:56
+@LastEditTime: 2020-05-01 00:50:42
 @Description: file content
 @FilePath: /Covo/Walfare/views.py
 '''
 from django.shortcuts import render
+from django.shortcuts import redirect
 from .models import Walfare
 # Create your views here.
 def get_walfarelist(request):
@@ -15,10 +16,39 @@ def get_walfarelist(request):
     return render(request, './Walfare/walfarelist.html', context)
 
 def to_walfaremanager(request):
-    return render(request, './Walfare/walfaremanager.html')
+    context = {}
+    context['walfarelist'] = Walfare.objects.all().values('prize_number','prize_name', 'prize_points')
+    # redirect('/Walfare/to_walfaremanager/')
+    return render(request, './Walfare/walfaremanager.html', context)
 
 def add_walfare(request):
     return render(request, './Walfare/add_walfare.html')
+
+def save_walfare(request):
+    if request.POST:
+        if request.POST['prize_number'] is not 0:
+            prize_name = request.POST['name']
+            prize_introduction = request.POST['introduction']
+            prize_points = request.POST['points']
+            Walfare.objects.create(prize_name=prize_name, prize_introduction=prize_introduction, prize_points=prize_points)
+        else:
+            prize_number = request.POST['prize_number']
+            walfare = Walfare.objects.get(prize_number=prize_number)
+            walfare.prize_name = request.POST['name']
+            walfare.prize_introduction = request.POST['introduction']
+            walfare.prize_points = request.POST['points']
+            walfare.save()
+        # return to_walfaremanager(request)
+        return redirect('/walfare/to_walfaremanager/')
+
+def edit_walfare(request, prize_number):
+    context = {}
+    context['walfareinfo'] = Walfare.objects.get(prize_number=prize_number)
+    return render(request, './Walfare/edit_walfare.html', context)
+
+def delete_walfare(request, prize_number):
+    Walfare.objects.get(prize_number=prize_number).delete()
+    return to_walfaremanager(request)
 
 def get_walfareinfo(request, prize_number):
     context = {}
